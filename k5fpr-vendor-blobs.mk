740668062bc838722e6f9d11e579e87a6743ec07
SahilSonar <sss.sonar2003@outlook.com>
2019-08-22 20:56:36 +0530

    vendor: Rework on vendor to implement new style
    * Change codename to k5fpr
    * Nuke some unused files
    
    Signed-off-by: SahilSonar <sss.sonar2003@outlook.com>

---
+++ k5fpr-vendor-blobs.mk
@@ -0,0 +1,1 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/k5fpr/vendor,$(TARGET_COPY_OUT_VENDOR))
---
+++ k5fpr-vendor.mk
@@ -0,0 +1,1 @@
+$(call inherit-product, vendor/lenovo/k5fpr/k5fpr-vendor-blobs.mk)
